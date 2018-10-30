ee2be4d73a869a4b5582d8a66a486e7665955f9e
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-10-30 18:19:01 +0200

    feat(*) add travis-ci integration

---
+++ .ci/run_tests.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+export BUSTED_ARGS="-o gtest -v --exclude-tags=flaky,ipv6"
+
+pushd $DOWNLOAD_CACHE/kong
+
+    if [ "$KONG_TEST_DATABASE" == "postgres" ]; then
+        export TEST_CMD="bin/busted $BUSTED_ARGS,cassandra"
+    elif [ "$KONG_TEST_DATABASE" == "cassandra" ]; then
+        export KONG_TEST_CASSANDRA_KEYSPACE=kong_tests
+        export KONG_TEST_DB_UPDATE_PROPAGATION=1
+        export TEST_CMD="bin/busted $BUSTED_ARGS,postgres"
+    fi
+
+    mkdir output
+    if [ "$TEST_SUITE" == "integration" ]; then
+        eval "$TEST_CMD" spec/02-integration/ | tee output/integration.txt
+    fi
+    if [ "$TEST_SUITE" == "plugins" ]; then
+        eval "$TEST_CMD" spec/03-plugins/ | tee output/plugins.txt
+    fi
+    if [ "$TEST_SUITE" == "old-integration" ]; then
+        eval "$TEST_CMD" spec-old-api/02-integration/ | tee output/old_integration.txt
+    fi
+    if [ "$TEST_SUITE" == "old-plugins" ]; then
+        eval "$TEST_CMD" spec-old-api/03-plugins/ | tee output/old_plugins.txt
+    fi
+    if [ "$TEST_SUITE" == "pdk" ]; then
+        TEST_NGINX_RANDOMIZE=1 prove -I. -j$JOBS -r t/01-pdk | tee output/pdk.txt
+    fi
+
+popd
